7bf0f5c4c75d8dbd7445c882d11e79f8a796b3b2
Hiten Parmar <hrparmar@gmail.com>
2012-12-25 07:23:12 -0600

    Clean up content script.

--- code/js/content.js
+++ code/js/content.js
@@ -1,37 +1,10 @@
 requirejs.config(requirejsConfig);
 
-requirejs(['jquery', 'config', 'util/messaging', 'util/messagingClient'],
-function(   $,        config,   messaging,        client) {
-
-  // uncomment the following line if content should be handling some requests
-  // sent from background (when appropriate handler is implemented in
-  // contentHandlers.js
-
-  // messaging.contentInitialize();
-
-  client.sendBroadcast({
-    cmd: 'GetHtml',
-    args: {
-      template: 'content',
-      data: {
-        imageUrl: chrome.extension.getURL('/images/icon.png')
-      }
-    }
-  }, function(response) {
-    $(function() {
-      $(response)
-        .hide()
-        .appendTo('body')
-        .fadeIn('slow')
-        .delay(config.showTime)
-        .fadeOut('slow', function() {
-          $(this).remove();
-        });
-    });
-  });
+requirejs(['jquery'],
+function(   $   ) {
 
   $(function() {
-    $('select').select2();
+    $('select:visible').select2();
   });
 
 });
