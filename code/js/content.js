f64cf93ca4970f6a1e89bd8d5f7811e3eb701e0d
Hiten Parmar <hrparmar@gmail.com>
2012-12-26 08:02:55 -0600

    Add a link to trigger Select2.

--- code/js/content.js
+++ code/js/content.js
@@ -3,8 +3,19 @@ requirejs.config(requirejsConfig);
 requirejs(['jquery'],
 function(   $   ) {
 
-  $(function() {
-    $('select:visible').select2();
-  });
+  var selectize = function() {
+    $(function() {
+      $('select:visible').select2();
+    })
+  };
+
+  if ($('select:visible').length > 0) {
+    $('<div style="position:absolute; top: 5px; right: 5px; background-color: pink; cursor: pointer">Select2</div>')
+      .appendTo('body')
+      .on('click', function() { 
+        selectize();
+        $(this).css('background-color', 'red');
+      });
+  }
 
 });
